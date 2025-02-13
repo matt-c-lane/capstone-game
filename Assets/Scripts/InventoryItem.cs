1d2254caf147f30d721496bee6c88898f7d977cd
NewloveD <danielnewlove32@gmail.com>
2025-02-13 16:32:13 -0500

    Start of dungeon Sprint and camera movement
    this update has a camera movement code and solid objects so that players cant go through some walls

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
-/*
-InventoryItem represents an item managed by an Inventory. You should not inherit directly from this class.
-Create a new ScriptableObject instead.
-*/
+
 [CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
 public class InventoryItem : ScriptableObject
 {
     public string itemName;
     public string description;
-    public int quantity;
+    public int quantity = 1; // Default value
     public Sprite icon;
-
-    public InventoryItem(string name, string desc, int qty = 1, Sprite icn)
-    {
-        itemName = name;
-        description = desc;
-        quantity = qty;
-        icon = icn;
-    }
 
     public override string ToString()
     {
         return $"{itemName} (x{quantity}): {description}";
+    }
+
+    // Factory method to create an item in runtime
+    public static InventoryItem Create(string name, string desc, Sprite icn, int qty = 1)
+    {
+        InventoryItem newItem = ScriptableObject.CreateInstance<InventoryItem>();
+        newItem.itemName = name;
+        newItem.description = desc;
+        newItem.icon = icn;
+        newItem.quantity = qty;
+        return newItem;
     }
 }
